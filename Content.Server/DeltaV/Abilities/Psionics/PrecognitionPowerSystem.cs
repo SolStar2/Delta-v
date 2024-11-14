e9711f7b2c435f1ee390da08bcf9afe51c3b62b5
SolStar2 <44028047+ewokswagger@users.noreply.github.com>
2024-11-13 20:42:27 -0500

    Precog get next event

--- Content.Server/DeltaV/Abilities/Psionics/PrecognitionPowerSystem.cs
+++ Content.Server/DeltaV/Abilities/Psionics/PrecognitionPowerSystem.cs
@@ -1,20 +1,29 @@
-using Content.Server.Body.Components;
+using Content.Server.Chat.Managers;
 using Content.Server.DoAfter;
+using Content.Server.GameTicking;
+using Content.Server.Mind;
 using Content.Shared.Abilities.Psionics;
 using Content.Shared.Actions.Events;
 using Content.Shared.Actions;
+using Content.Shared.DeltaV.StationEvents;
 using Content.Shared.DoAfter;
 using Content.Shared.Psionics.Events;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 
 namespace Content.Server.Abilities.Psionics
 {
     public sealed class PrecognitionPowerSystem : EntitySystem
     {
+        [Dependency] public readonly GameTicker GameTicker = default!;
         [Dependency] private readonly SharedActionsSystem _actions = default!;
         [Dependency] private readonly SharedPsionicAbilitiesSystem _psionics = default!;
         [Dependency] private readonly DoAfterSystem _doAfterSystem = default!;
+        [Dependency] private readonly MindSystem _mind = default!;
+        [Dependency] private readonly IComponentFactory _factory = default!;
+        [Dependency] private readonly IChatManager _chat = default!;
         [Dependency] private readonly IGameTiming _gameTiming = default!;
+        [Dependency] private readonly IPrototypeManager _prototype = default!;
 
         public override void Initialize()
         {
@@ -63,6 +72,37 @@ namespace Content.Server.Abilities.Psionics
         private void OnDoAfter(EntityUid uid, PrecognitionPowerComponent component, PrecognitionDoAfterEvent args)
         {
             component.DoAfter = null;
+        }
+    private NextEventComponent? FindNextEvent(TimeSpan minDetectWindow, TimeSpan maxDetectWindow)
+        {
+            NextEventComponent? earliestNextEvent = null;
+            TimeSpan? earliestNextEventTime = null;
+            var query = EntityQueryEnumerator<NextEventComponent>();
+            while (query.MoveNext(out var uid, out var nextEventComponent))
+            {
+                // Update if the event is the most recent event that isnt too close or too far from happening to be of use
+                if (nextEventComponent.NextEventTime > GameTicker.RoundDuration() + minDetectWindow
+                    && nextEventComponent.NextEventTime < GameTicker.RoundDuration() + maxDetectWindow
+                    && nextEventComponent.NextEventTime < earliestNextEventTime)
+                    earliestNextEvent ??= nextEventComponent;
+            }
+            return earliestNextEvent;
+        }
+        public Dictionary<EntityPrototype, PrecognitionResultComponent> AllPrecognitionResults()
+        {
+            var allEvents = new Dictionary<EntityPrototype, PrecognitionResultComponent>();
+            foreach (var prototype in _prototype.EnumeratePrototypes<EntityPrototype>())
+            {
+                if (prototype.Abstract)
+                    continue;
+
+                if (!prototype.TryGetComponent<PrecognitionResultComponent>(out var precognitionResult, _factory))
+                    continue;
+
+                allEvents.Add(prototype, precognitionResult);
+            }
+
+            return allEvents;
         }
     }
 }
